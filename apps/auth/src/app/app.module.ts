a3964d4eb26132189cfac6aae18d6f6e08396afc
aliaksei20-10 <aliaksei.kulheika@yellow.systems>
2025-03-26 18:11:21 +0100

    feat: basi logic

--- apps/auth/src/app/app.module.ts
+++ apps/auth/src/app/app.module.ts
@@ -2,12 +2,21 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-
+import { UserModule } from './users/user.module';
 @Module({
-  imports: [PrismaModule, GraphQLModule.forRoot<ApolloDriverConfig>({
-    driver: ApolloDriver,
-    autoSchemaFile: true,
-  }),
+  imports: [
+    PrismaModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: {
+        settings: {
+          'request.credentials': 'include',
+        },
+      },
+      context: ({ req, res }) => ({ req, res }),
+      autoSchemaFile: true,
+    }),
+    UserModule,
   ],
 })
 
